61beb1e9c20ef057c59ff558928bb6836f2bbf53
PJunhyuk-mac <jgravity@naver.com>
2017-07-13 23:29:46 -0700

    Remove function items

--- routes/blend.js
+++ routes/blend.js
@@ -6,7 +6,6 @@ var Schema = mongoose.Schema;
 //메모 모델을 정의합니다.
 var Blend = new Schema({
 	app_title: String,
-	app_function1: Boolean,
 	app_function2: Boolean,
 	app_function3: Boolean,
 	app_color: String,
@@ -33,9 +32,6 @@ exports.load = function(req, res) {
 //메모 쓰기 요청을 처리합니다.
 exports.write = function(req, res) {
 	var app_title = req.body.app_title;
-	var app_function1 = req.body.app_function1;
-	var app_function2 = req.body.app_function2;
-	var app_function3 = req.body.app_function3;
 	var app_color = req.body.app_color;
 	var user_phone = req.body.user_phone;
 
@@ -45,9 +41,6 @@ exports.write = function(req, res) {
 	var blend = new blendModel();
 
 	blend.app_title = app_title;
-	blend.app_function1 = app_function1;
-	blend.app_function2 = app_function2;
-	blend.app_function3 = app_function3;
 	blend.app_color = app_color;
 	blend.user_phone = user_phone;
 	blend.contents = contents;
